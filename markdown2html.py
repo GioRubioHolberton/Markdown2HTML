3e1e6504931f4e3bf0e564d1338c4950018ae63b
GioRubioHolberton <759@holbertonschool.com>
2020-06-13 16:12:50 +0000

    pep8 exe 0

--- markdown2html.py
+++ markdown2html.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ script that takes an argument 2 strings  """
 
-if __name__=='__main__':
+if __name__ == '__main__':
+
     import sys
     from os import path
-    
+
     if len(sys.argv) < 2:
         sys.stderr.write('Usage: ./markdown2html.py README.md README.html\n')
         exit(1)
